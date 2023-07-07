228017d882a030f5f864a34a61a72fc224784b63
navvay <135510092+navvay@users.noreply.github.com>
2023-07-07 14:34:25 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -10,16 +10,16 @@ for DEPENDENCY in $DEPENDENCIES; do
     # Extract dependency name and version
     DEP_NAME=$(echo $DEPENDENCY | cut -d: -f1)
     DEP_VERSION=$(echo $DEPENDENCY | cut -d: -f2)
-    
+
     # Extract the repository owner and name from the GitHub URL
     REPO_OWNER=$(echo $DEP_NAME | cut -d/ -f4)
     REPO_NAME=$(echo $DEP_NAME | cut -d/ -f5)
 
-    # Retrieve the latest version from the GitHub API
-    LATEST_VERSION=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest" | jq -r '.tag_name')
+    # Retrieve the latest version tag from the GitHub API
+    LATEST_TAG=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/tags" | jq -r '.[0].name')
 
-    if [ "$LATEST_VERSION" != "null" ] && [ "$LATEST_VERSION" != "$DEP_VERSION" ]; then
-        OUTDATED_VERSIONS+="\n$DEP_NAME: $DEP_VERSION -> $LATEST_VERSION"
+    if [ "$LATEST_TAG" != "null" ] && [ "$LATEST_TAG" != "$DEP_VERSION" ]; then
+        OUTDATED_VERSIONS+="\n$DEP_NAME: $DEP_VERSION -> $LATEST_TAG"
     fi
 done
 
