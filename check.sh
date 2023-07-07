0bc79b46f21d1b2ec2548f7fb8cc191391dccae9
navvay <135510092+navvay@users.noreply.github.com>
2023-07-07 14:56:38 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
-# Parse Package.swift
-PACKAGE_FILE="Package.swift"
-DEPENDENCIES=$(awk '/dependencies:/,/\]/ { if ($0 ~ /url:/) { getline; gsub("\"", ""); print $2 ":" $4 } }' $PACKAGE_FILE)
+# Function to extract the latest version of a package from the Swift Package Manager dump
+function get_latest_version {
+    package_name=$1
+    package_dump=$2
+    latest_version=$(echo "$package_dump" | grep -A1 "\"name\": \"$package_name\"" | grep "\"version\"" | awk -F'"' '{print $4}')
+    echo "$latest_version"
+}
 
-# Check for the latest version of each dependency
-OUTDATED_VERSIONS=""
-for DEPENDENCY in $DEPENDENCIES; do
-    # Extract dependency name and version
-    DEP_NAME=$(echo $DEPENDENCY | cut -d: -f1)
-    DEP_VERSION=$(echo $DEPENDENCY | cut -d: -f2)
+# Extract and check the latest version for each dependency
+check_dependency() {
+    package_name=$1
+    package_url=$2
+    package_requirement=$3
 
-    # Extract the repository owner and name from the dependency URL
-    REPO_OWNER=$(echo $DEP_NAME | cut -d/ -f4)
-    REPO_NAME=$(echo $DEP_NAME | cut -d/ -f5)
+    echo "Checking $package_name..."
 
-    # Retrieve the latest tags from the GitHub API
-    LATEST_TAG=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/tags" | jq -r '.[0].name')
+    # Clone the repository to get the latest tags
+    git clone --depth=1 $package_url temp_repository
+    cd temp_repository
 
-    if [ "$LATEST_TAG" != "null" ] && [ "$LATEST_TAG" != "$DEP_VERSION" ]; then
-        OUTDATED_VERSIONS+="\n$DEP_NAME: $DEP_VERSION -> $LATEST_TAG"
-    fi
-done
+    # Get the latest version from the package dump
+    package_dump=$(swift package dump-package)
+    latest_version=$(get_latest_version "$package_name" "$package_dump")
 
-if [ -n "$OUTDATED_VERSIONS" ]; then
-    echo -e "Outdated versions:$OUTDATED_VERSIONS"
-else
-    echo "All dependencies are up to date!"
-fi
+    # Print the latest version
+    echo "Latest version of $package_name: $latest_version"
+
+    cd ..
+    rm -rf temp_repository
+}
+
+# Read the Package.swift file and extract dependencies
+dependencies=$(awk '/\.package\(url:/{flag=1;next}/\),/{flag=0}flag' Package.swift)
+
+while IFS= read -r dependency; do
+    package_name=$(echo "$dependency" | grep -oP '(?<=name: ")[^"]+')
+    package_url=$(echo "$dependency" | grep -oP '(?<=url: ")[^"]+')
+    package_requirement=$(echo "$dependency" | grep -oP '(?<=from: ")[^"]+')
+
+    check_dependency "$package_name" "$package_url" "$package_requirement"
+done <<< "$dependencies"
