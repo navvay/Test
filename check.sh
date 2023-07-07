c9929de3a3c5e0c3d94c461713188f5020280b9d
navvay <135510092+navvay@users.noreply.github.com>
2023-07-07 15:00:59 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,43 +1,22 @@
 #!/bin/bash
 
-# Function to extract the latest version of a package from the Swift Package Manager dump
-function get_latest_version {
-    package_name=$1
-    package_dump=$2
-    latest_version=$(echo "$package_dump" | grep -A1 "\"name\": \"$package_name\"" | grep "\"version\"" | awk -F'"' '{print $4}')
-    echo "$latest_version"
-}
+# Read the Package.swift file
+while read -r line; do
+  # Check if the line contains the dependency URL
+  if [[ $line == *".package(url:"* ]]; then
+    # Extract the URL and version range
+    url=$(echo "$line" | grep -o '".*"' | sed 's/"//g')
+    version_range=$(echo "$line" | grep -o 'from:.*' | awk '{print $2}')
 
-# Extract and check the latest version for each dependency
-check_dependency() {
-    package_name=$1
-    package_url=$2
-    package_requirement=$3
+    # Extract the package name from the URL
+    package_name=$(basename "$url" .git)
 
-    echo "Checking $package_name..."
+    # Check the latest version available
+    latest_version=$(git ls-remote --tags "$url" | grep -Eo "refs/tags/[0-9.]+" | awk -F/ '{print $NF}' | sort -V | tail -n1)
 
-    # Clone the repository to get the latest tags
-    git clone --depth=1 $package_url temp_repository
-    cd temp_repository
-
-    # Get the latest version from the package dump
-    package_dump=$(swift package dump-package)
-    latest_version=$(get_latest_version "$package_name" "$package_dump")
-
-    # Print the latest version
-    echo "Latest version of $package_name: $latest_version"
-
-    cd ..
-    rm -rf temp_repository
-}
-
-# Read the Package.swift file and extract dependencies
-dependencies=$(awk '/\.package\(url:/{flag=1;next}/\),/{flag=0}flag' Package.swift)
-
-while IFS= read -r dependency; do
-    package_name=$(echo "$dependency" | grep -oP '(?<=name: ")[^"]+')
-    package_url=$(echo "$dependency" | grep -oP '(?<=url: ")[^"]+')
-    package_requirement=$(echo "$dependency" | grep -oP '(?<=from: ")[^"]+')
-
-    check_dependency "$package_name" "$package_url" "$package_requirement"
-done <<< "$dependencies"
+    # Print the package name and latest version
+    echo "Package: $package_name"
+    echo "Latest Version: $latest_version"
+    echo
+  fi
+done <Package.swift
