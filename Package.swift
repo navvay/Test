9fa555b9e29b82716afdc434040ccef2f405534e
navvay <135510092+navvay@users.noreply.github.com>
2023-07-07 12:48:30 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,37 +1,37 @@
-// swift-tools-version:5.3
-
-/*
- This source file is part of the Swift.org open source project
-
- Copyright 2015 – 2021 Apple Inc. and the Swift project authors
- Licensed under Apache License v2.0 with Runtime Library Exception
-
- See http://swift.org/LICENSE.txt for license information
- See http://swift.org/CONTRIBUTORS.txt for Swift project authors
-*/
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
-    name: "DeckOfPlayingCards",
+    name: "MyApp",
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v15),
+        .watchOS(.v8),
+        .tvOS(.v15)
+    ],
     products: [
-        .library(name: "DeckOfPlayingCards", targets: ["DeckOfPlayingCards"]),
+        .library(name: "MyLibrary", targets: ["MyLibrary"])
     ],
     dependencies: [
-        .package(name: "PlayingCard",
-                 url: "https://github.com/apple/example-package-playingcard.git",
-                 from: "3.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.12.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.2.0")
     ],
     targets: [
         .target(
-            name: "DeckOfPlayingCards",
+            name: "MyLibrary",
             dependencies: [
-                .byName(name: "PlayingCard")
-            ]),
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "SDWebImage", package: "SDWebImage"),
+                .product(name: "RxSwift", package: "RxSwift")
+            ],
+            path: "Sources/MyLibrary"
+        ),
         .testTarget(
-            name: "DeckOfPlayingCardsTests",
-            dependencies: [
-                .target(name: "DeckOfPlayingCards")
-            ]),
+            name: "MyLibraryTests",
+            dependencies: ["MyLibrary"],
+            path: "Tests/MyLibraryTests"
+        )
     ]
 )
