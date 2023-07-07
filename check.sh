64e07abd6bdef0247fa70ed803dcbac94157314a
navvay <135510092+navvay@users.noreply.github.com>
2023-07-07 14:44:46 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,7 +2,7 @@
 
 # Parse Package.swift
 PACKAGE_FILE="Package.swift"
-DEPENDENCIES=$(awk '/dependencies:/,/\]/ { if ($0 ~ /url:/) { getline; gsub("\"", ""); print $2 ":" $4 } }' $PACKAGE_FILE)
+DEPENDENCIES=$(awk '/dependencies:/,/]/ { if ($0 ~ /url:/) { getline; gsub("\"", ""); print $2 ":" $4 } }' $PACKAGE_FILE)
 
 # Check for the latest version of each dependency
 OUTDATED_VERSIONS=""
@@ -11,15 +11,11 @@ for DEPENDENCY in $DEPENDENCIES; do
     DEP_NAME=$(echo $DEPENDENCY | cut -d: -f1)
     DEP_VERSION=$(echo $DEPENDENCY | cut -d: -f2)
 
-    # Extract the repository owner and name from the dependency URL
-    REPO_OWNER=$(echo $DEP_NAME | cut -d/ -f4)
-    REPO_NAME=$(echo $DEP_NAME | cut -d/ -f5)
+    # Retrieve the latest version from the Swift Package Index API
+    LATEST_VERSION=$(curl -s "https://api.swiftpackageindex.com/package/$DEP_NAME" | jq -r '.versions | keys_unsorted | .[-1]')
 
-    # Retrieve the latest version tag from the GitHub API
-    LATEST_TAG=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest" | jq -r '.tag_name')
-
-    if [ "$LATEST_TAG" != "null" ] && [ "$LATEST_TAG" != "$DEP_VERSION" ]; then
-        OUTDATED_VERSIONS+="\n$DEP_NAME: $DEP_VERSION -> $LATEST_TAG"
+    if [ "$LATEST_VERSION" != "null" ] && [ "$LATEST_VERSION" != "$DEP_VERSION" ]; then
+        OUTDATED_VERSIONS+="\n$DEP_NAME: $DEP_VERSION -> $LATEST_VERSION"
     fi
 done
 
