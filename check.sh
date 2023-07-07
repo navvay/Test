2319847630e9346cad22582966f59356a37a949d
navvay <135510092+navvay@users.noreply.github.com>
2023-07-07 14:21:32 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -5,12 +5,7 @@ PACKAGE_FILE="Package.swift"
 DEPENDENCIES=$(sed -n '/dependencies:/,/]/p' $PACKAGE_FILE | awk -F'"' '/url:/{print $2 ":" $4}')
 
 # Check for the latest version of each dependency
-MISSING_VERSIONS="missing_versions.txt"
-> $MISSING_VERSIONS
-
-OUTDATED_VERSIONS="outdated_versions.txt"
-> $OUTDATED_VERSIONS
-
+OUTDATED_VERSIONS=""
 for DEPENDENCY in $DEPENDENCIES; do
     # Extract dependency name and version
     DEP_NAME=$(echo $DEPENDENCY | cut -d: -f1)
@@ -20,14 +15,12 @@ for DEPENDENCY in $DEPENDENCIES; do
     LATEST_VERSION=$(curl -s "https://api.swiftpackageindex.com/package/$DEP_NAME" | jq -r '.versions | keys | .[-1]')
 
     if [ "$LATEST_VERSION" != "$DEP_VERSION" ]; then
-        echo "$DEP_NAME: $LATEST_VERSION" >> $MISSING_VERSIONS
-        echo "$DEP_NAME: $DEP_VERSION -> $LATEST_VERSION" >> $OUTDATED_VERSIONS
+        OUTDATED_VERSIONS+="\n$DEP_NAME: $DEP_VERSION -> $LATEST_VERSION"
     fi
 done
 
-if [ -s $MISSING_VERSIONS ]; then
-    echo "Missing versions written to $MISSING_VERSIONS"
-    cat $OUTDATED_VERSIONS
+if [ -n "$OUTDATED_VERSIONS" ]; then
+    echo -e "Outdated versions:$OUTDATED_VERSIONS"
 else
     echo "All dependencies are up to date!"
 fi
