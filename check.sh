bac846e0224b23a7494f4ddce9999ee2d7904d23
navvay <135510092+navvay@users.noreply.github.com>
2023-07-07 14:18:35 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -8,6 +8,9 @@ DEPENDENCIES=$(sed -n '/dependencies:/,/]/p' $PACKAGE_FILE | awk -F'"' '/url:/{p
 MISSING_VERSIONS="missing_versions.txt"
 > $MISSING_VERSIONS
 
+OUTDATED_VERSIONS="outdated_versions.txt"
+> $OUTDATED_VERSIONS
+
 for DEPENDENCY in $DEPENDENCIES; do
     # Extract dependency name and version
     DEP_NAME=$(echo $DEPENDENCY | cut -d: -f1)
@@ -18,11 +21,13 @@ for DEPENDENCY in $DEPENDENCIES; do
 
     if [ "$LATEST_VERSION" != "$DEP_VERSION" ]; then
         echo "$DEP_NAME: $LATEST_VERSION" >> $MISSING_VERSIONS
+        echo "$DEP_NAME: $DEP_VERSION -> $LATEST_VERSION" >> $OUTDATED_VERSIONS
     fi
 done
 
 if [ -s $MISSING_VERSIONS ]; then
     echo "Missing versions written to $MISSING_VERSIONS"
+    cat $OUTDATED_VERSIONS
 else
     echo "All dependencies are up to date!"
 fi
